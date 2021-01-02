311f1a056c12d5b5765a1c0d8bd555ad13a1794d
hoborm <hoborm0@gmail.com>
2021-01-02 15:51:55 +0100

    Add login without cookie accept support and without 2FA support

---
+++ deleteFacebookPosts/FacebookAutomation.cs
@@ -0,0 +1,55 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace deleteFacebookPosts
+{
+    class FacebookAutomation
+    {
+        private readonly IWebDriver webDriver;
+
+        public FacebookAutomation(IWebDriver webDriver)
+        {
+            this.webDriver = webDriver;
+        }
+
+        public void Login(string username, string password)
+        {
+            // Navigate to Facebook
+            webDriver.Url = "https://www.facebook.com/";
+
+            // Find the username field (Facebook calls it "email") and enter value
+            var input = webDriver.FindElement(By.Id("email"));
+            input.SendKeys(username);
+
+            // Find the password field and enter value
+            input = webDriver.FindElement(By.Id("pass"));
+            input.SendKeys(password);
+
+            // Click on the login button
+            //ClickAndWaitForPageToLoad(webDriver, By.Id("u_0_b"));
+            webDriver.FindElement(By.Id("u_0_b")).Click();
+            
+            // At this point, Facebook will launch a post-login "wizard" that will 
+            // keep asking unknown amount of questions (it thinks it's the first time 
+            // you logged in using this computer). We'll just click on the "continue" 
+            // button until they give up and redirect us to our "wall".
+            try
+            {
+                while (webDriver.FindElement(By.Id("checkpointSubmitButton")) != null)
+                {
+                    // Clicking "continue" until we're done
+                }
+            }
+            catch
+            {
+                // We will try to click on the next button until it's not there or we fail.
+                // Facebook is unexpected as to what will happen, but this approach seems 
+                // to be pretty reliable
+            }
+        }
+    }
+}
--- deleteFacebookPosts/Program.cs
+++ deleteFacebookPosts/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using System;
 
 namespace deleteFacebookPosts
 {
@@ -6,7 +10,30 @@ namespace deleteFacebookPosts
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var webDriver = LaunchBrowser();
+            try
+            {
+                var facebookAutomation = new FacebookAutomation(webDriver);
+                facebookAutomation.Login("my username", "my pass");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error while executing automation");
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                webDriver.Quit();
+            }
+        }
+        static IWebDriver LaunchBrowser()
+        {
+            var options = new ChromeOptions();
+            options.AddArgument("--start-maximized");
+            options.AddArgument("--disable-notifications");
+
+            var driver = new ChromeDriver(options);
+            return driver;
         }
     }
 }
